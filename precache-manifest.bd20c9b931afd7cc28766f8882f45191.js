f5dcb2cd94bb49d56831f738b16a054f2da59b68
olumorin samuel <olumorinsammy@gmail.com>
2020-02-16 16:29:15 -0800

    Updates

---
+++ precache-manifest.bd20c9b931afd7cc28766f8882f45191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda48f66f816fd8a91d9ced50c56e979",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e96ab8b505b6a4b953ad",
+    "url": "/monsters-rolodex/static/css/main.a6abcd4e.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96ab8b505b6a4b953ad",
+    "url": "/monsters-rolodex/static/js/main.2802a7ff.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
